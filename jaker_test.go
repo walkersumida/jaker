5e76859226517f0b6d30185da701cc0c2e9fe886
WalkerSumida <walkersumida@gmail.com>
2021-09-19 21:48:51 +0900

    refactor: move tests

--- jaker_test.go
+++ jaker_test.go
@@ -2,22 +2,6 @@ package jaker
 
 import "testing"
 
-func TestFirstName(t *testing.T) {
-	expected := 1
-	got := len(PickUpFirstName().En)
-	if got < expected {
-		t.Errorf("\nexpected: %d\n     got: %d", expected, got)
-	}
-}
-
-func TestLastName(t *testing.T) {
-	expected := 1
-	got := len(PickUpLastName().En)
-	if got < expected {
-		t.Errorf("\nexpected: %d\n     got: %d", expected, got)
-	}
-}
-
 func TestProfile(t *testing.T) {
 	expected := 1
 
